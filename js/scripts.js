2f921b4c658190cc735a40ecf3d2565b6c395d11
Yazmin Perez <zoeperez333@gmail.com>
2022-02-12 19:14:25 -0800

    create assignId method

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
+// Business Logic for AddressBook ---------
 function AddressBook(){
   this.contacts = {};
+  this.currentId = 0;
 }
 
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts[contact.firstName] = contact;
+};
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
