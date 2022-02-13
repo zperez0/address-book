6633b87bd2985b894dfc67bbe55cbb3b4e40b3f8
Yazmin Perez <zoeperez333@gmail.com>
2022-02-12 17:50:57 -0800

    create AddressBook constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
+function AddressBook(){
+  this.contacts = {};
+}
+
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
 
-//to define a new prototype function
-// method created to return contacts first & last name concat together
 Contact.prototype.fullName = function() {
   return this.firstName + ' ' + this.lastName;
 };
-
